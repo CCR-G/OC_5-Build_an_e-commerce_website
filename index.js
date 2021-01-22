19ec3f92e28dcd09b83452508dac7ff0f0412bc6
CCR-G <>
2021-01-22 14:04:59 +0100

    Generating item list on homepage
    Creation of the main function generateItemsListPage() which retrieves
    the list of items and insert each of them in the page as a card.
    
    Supporting functions newly added are:
    * generateFurnituresList()
    * insertItemCardsInPage()
    * createFurnitureCard()

---
+++ index.js
@@ -0,0 +1,51 @@
+generateItemsListPage();
+
+function generateItemsListPage() {
+    getFurnituresList()
+        .catch((error) => {
+            console.log(error.message);
+        })
+        .then((furnitures_list) => {
+            insertItemCardsInPage(furnitures_list);
+        });
+}
+
+async function getFurnituresList() {
+    const furnitures_list = await fetch(`http://localhost:3000/api/furniture`);
+
+    if (!furnitures_list.ok) {
+        throw new Error(`Error ${furnitures_list.status} : List of item could not be retrieved.`);
+    }
+
+    const json_list = await furnitures_list.json();
+
+    return json_list;
+}
+
+function insertItemCardsInPage(furnitures_list) {
+    const item_cards_container = getDataElement("item-cards-container");
+    furnitures_list.forEach((item) => {
+        item_cards_container.appendChild(createFurnitureCard(item));
+    });
+}
+
+function createFurnitureCard(item) {
+    let item_card_template = getDataElement("item-card-template");
+
+    let item_card_iteration = document.importNode(item_card_template.content, true);
+
+    let card_name = getDataElement("item-name", item_card_iteration);
+    card_name.textContent = item.name;
+
+    let card_price = getDataElement("item-price", item_card_iteration);
+    card_price.textContent = item.price;
+
+    let card_link = getDataElement("item-link", item_card_iteration);
+    card_link.href = `item.html?id=${item._id}`;
+    card_link.title = `Naviguer vers la page ${item.name}`;
+
+    let card_image = getDataElement("item-image", item_card_iteration);
+    card_image.src = item.imageUrl;
+
+    return item_card_iteration;
+}
