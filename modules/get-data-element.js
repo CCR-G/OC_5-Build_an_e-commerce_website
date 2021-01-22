848aaaff853707cb0802f3cb32dc4d9db859c146
CCR-G <>
2021-01-22 11:04:49 +0100

    Creating the getDataElement() function
    This function will be used in all pages to retrieves elements that will
    be replaced using JavaScript.
    
    It makes sure the element is retrieved and throws an error otherwise.
    This function avoids repeating the existence assertion when querying a
    element (DRY).
    
    It also marks the importance of using the `data` attribute on elements that will be edited by JS files.
    Thus, when editing the HTML file, one must be careful with element bearing a
    `data` attribute.

---
+++ modules/get-data-element.js
@@ -0,0 +1,20 @@
+/**
+ * Retrieves the first HTML element corresponding to the data attribute's value passed as argument and throws an error otherwise.
+ * The search scope can be an HTMLElement or defaults to the entire Document.
+ *
+ * @function getDataElement
+ * @param {string} data_attribute_value - Value given to the data attribute of the element to look for
+ * @param {HTMLElement} [search_scope=document] - Where to look for the data attribute, defaults to the entire Document
+
+ * @returns {HTMLElement} First element matching the data attribute's value
+ */
+
+function getDataElement(data_attribute_value, search_scope = document) {
+    const element = search_scope.querySelector(`[data='${data_attribute_value}']`);
+
+    if (!(element instanceof HTMLElement)) {
+        throw new Error(`data='${data_attribute_value}' could not be found or is not an HTMLElement`);
+    }
+
+    return element;
+}
